886a836205f313cd19e91788a48a25248a6502a9
Wang <msj1013654754@gmail.com>
2019-12-31 15:44:31 +0800

    Update whiteList.java
    添加注释

--- whiteList.java
+++ whiteList.java
@@ -1,17 +1,19 @@
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
- * @Author: zerorooot
+ * @Author: zero
  * @Date: 2019/12/26 9:12
  */
 public class whiteList {
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
-            System.out.println("java whiteList.java <socks5_addr:socks5_port>");
+            System.out.println("sudo java whiteList.java <socks5_addr:socks5_port>");
             return;
         }
+
         String addr = args[0];
         String filePath = "/etc/privoxy/whitelist.action";
         String urlName = "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf";
@@ -27,11 +29,22 @@ public class whiteList {
         HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
         httpsURLConnection.setRequestProperty("Cache-Control", "no-cache");
         httpsURLConnection.setRequestProperty("Charset", "UTF-8");
-        httpsURLConnection.connect();
+        httpsURLConnection.setConnectTimeout(2000);
+
+        //下载
+        try {
+            httpsURLConnection.connect();
+        } catch (Exception e) {
+            System.out.println("网络错误\n该死的gfw...( ＿ ＿)ノ｜");
+            return;
+        }
+        
+        //读取
         InputStream inputStream = httpsURLConnection.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
+        
+        //替换
         String line;
         StringBuffer stringBuffer = new StringBuffer(head);
         while ((line = bufferedReader.readLine()) != null) {
@@ -41,11 +54,22 @@ public class whiteList {
         bufferedReader.close();
         httpsURLConnection.disconnect();
 
+        //写入
         FileOutputStream fileOutputStream = new FileOutputStream(filePath);
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
         BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
         bufferedWriter.write(stringBuffer.toString());
         bufferedWriter.close();
-        System.out.println("ok~");
+        
+        //重启privoxy
+        String restart = " systemctl restart privoxy.service ";
+        try {
+            Runtime.getRuntime().exec(restart).waitFor(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println("重启privoxy失败，可能是权限不够或者没安装造成的\n(；′⌒`)");
+        }
+        
+        
+        System.out.println("安装成功\nd=====(￣▽￣*)b");
     }
 }
